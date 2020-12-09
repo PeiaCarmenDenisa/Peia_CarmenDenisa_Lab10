f414c1542ca46cd2ca171bbb3c63c342606be8b2
CARMEN-DENISA PEIA <carmen.peia@stud.ubbcluj.ro>
2020-12-09 16:48:37 +0200

    Add project files.

---
+++ Peia_CarmenDenisa_Lab10/Models/ShopList.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Peia_CarmenDenisa_Lab10.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
